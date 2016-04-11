f75da74b720f42421efc80ede4f9b323e22878a5
jop0995 <jms0995@hanmail.net>
2016-04-11 16:28:22 +0900

    6주차 더하기

---
+++ w6main4.py
@@ -0,0 +1,16 @@
+﻿j=list()
+
+def sumlist(j):
+     for i in range(0.1001):
+         if i%4==0 and i%5!=0:
+             j.append(i)
+     sum=0
+     for i in range(len(j)):
+         sum=sum+j[i]
+     return sum
+
+
+def main():
+     print sumlist(j)
+main()
+raw_input("enter to end")
