8addaa1693263b3101d4769c382048b8967a8913
201415059 _장민섭 <jms0995@hanmail.net>
2016-05-03 23:54:03 +0900

    Added files via upload

---
+++ w8main1-그래프.py
@@ -0,0 +1,19 @@
+﻿word='this is sangmyung university'
+words=word.split()
+mydict=dict()
+import matplotlib
+import matplotlib.pyplot as plt
+def lab8():	
+	for c in word:
+    		if c not in mydict:
+        		mydict[c]=1
+        		print mydict
+    		else:
+        		mydict[c]=mydict[c]+1
+	print len(mydict)
+	print mydict.keys()
+	print mydict.values()
+	plt.bar(range(len(mydict)), mydict.values() ,align='center')
+	plt.xticks(range(len(mydict)),list(mydict.keys()))
+	plt.show()
+lab8()
