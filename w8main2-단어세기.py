639a00c93a3e9d4d057aac3ce78443584695f929
201415059 _장민섭 <jms0995@hanmail.net>
2016-05-03 23:54:23 +0900

    Added files via upload

---
+++ w8main2-단어세기.py
@@ -0,0 +1,28 @@
+﻿word='hongjimoon2gil20'
+words=word.split()
+d=dict()
+wcount=0
+ncount=0
+for c in word:
+        if c not in d:
+            d[c]=1
+            print d
+        else:
+            d[c]=d[c]+1
+for c in d:
+    if c.isdigit()and d[c]==1:
+        ncount=ncount+1
+    elif c.isdigit()and d[c]==2:
+        ncount=ncount+2    
+    elif c.isalpha()and d[c]==1:
+        wcount=wcount+1
+    elif c.isalpha()and d[c]==2:
+        wcount=wcount+2
+    elif c.isalpha()and d[c]==3:
+        wcount=wcount+3
+    else:
+        wcount=wcount
+        ncount=ncount
+print wcount
+print ncount
+input()
---
+++ w8main3-가전제품찾기.py
@@ -0,0 +1,15 @@
+﻿myhome=list()
+friendhome=list()
+myhome=['tv','phone','camera','fridger','mixer','audio','cdplayer','light','computer','notebook','recorder']
+friendhome=['coffeemachine','radio','camera','runningmachine','ramp','computer','notebook','recorder']
+print myhome
+print friendhome
+k=friendhome
+k1=set(myhome)&set(friendhome)
+x1=list()
+print k1
+for c in friendhome:
+    if c in k and c not in k1:
+        x1.append(c)
+print "the thing that is not in your home and that is in your friendhome is" , x1 
+input()
