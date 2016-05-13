44fe83677d0e67671059b1f3e1423997ceb945dd
201415059 _장민섭 <jms0995@hanmail.net>
2016-05-13 11:57:13 +0900

    Add files via upload

---
+++ w11main-speech.py
@@ -0,0 +1,104 @@
+﻿import operator
+
+
+
+
+def Speech():
+    speech=[
+   "Hello, Chicago."
+
+"If there is anyone out there who still doubts that America is a place where all things are possible;" 
+"who still wonders if the dream of our founders is alive in our time;"
+"who still questions the power of our democracy, tonight is your answer."
+"It's the answer told by lines that stretched around schools and churches in numbers this nation has never seen; by people who waited three hours and four hours, many for the very first time in their lives, because they believed that this time must be different; that their voices could be that difference." 
+"It's the answer spoken by young and old, rich and poor, Democrat and Republican, black, white, Hispanic, Asian, Native American, gay, straight, disabled and not disabled -- Americans who sent a message to the world that we have never been just a collection of individuals or a collection of Red States and Blue States: we are, and always will be, the United States of America!"
+"It's the answer that -- that led those who have been told for so long by so many to be cynical, and fearful, and doubtful about what we can achieve to put their hands on the arc of history and bend it once more toward the hope of a better day."
+"It's been a long time coming, but tonight, because of what we did on this day, in this election, at this defining moment, change has come to America."
+"A little bit earlier this evening, I received an extraordinarily gracious call from Senator McCain. Senator McCain fought long and hard in this campaign, and he's fought even longer and harder for the country that he loves. He has endured sacrifices for America that most of us cannot begin to imagine. We are better off for the service rendered by this brave and selfless leader. I congratulate him; I congratulate Governor Palin for all that they've achieved, and I look forward to working with them to renew this nation's promise in the months ahead."
+"I want to thank my partner in this journey, a man who campaigned from his heart and spoke for the men and women he grew up with on the streets of Scranton and rode with on the train home to Delaware, the Vice President-elect of the United States, Joe Biden." 
+"And I would not be standing here tonight without the unyielding support of my best friend for the last 16 years, the rock of our family, the love of my life, the nation's next First Lady: Michelle Obama. Sasha and Malia, I love you both more than you can imagine, and you have earned the new puppy that's coming with us to the White House. And while she's no longer with us, I know my grandmother's watching, along with the family that made me who I am. I miss them tonight, and I know that my debt to them is beyond measure. To my sister Maya, my sister Alma, all my other brothers and sisters -- thank you so much for the support that you've given me. I am grateful to them."
+"And to my campaign manager, David Plouffe -- the unsung hero of this campaign, who built the best -- the best political campaign, I think, in the history of the United States of America. To my chief strategist David Axelrod -- who's been a partner with me every step of the way. To the best campaign team ever assembled in the history of politics -- you made this happen, and I am forever grateful for what you've sacrificed to get it done."
+"But above all, I will never forget who this victory truly belongs to. It belongs to you. It belongs to you. I was never the likeliest candidate for this office. We didn't start with much money or many endorsements. Our campaign was not hatched in the halls of Washington. It began in the backyards of Des Moines and the living rooms of Concord and the front porches of Charleston. It was built by working men and women who dug into what little savings they had to give 5 dollars and 10 dollars and 20 dollars to the cause. It grew strength from the young people who rejected the myth of their generation's apathy, who left their homes and their families for jobs that offered little pay and less sleep. It drew strength from the not-so-young people who braved the bitter cold and scorching heat to knock on doors of perfect strangers,"
+"and from the millions of Americans who volunteered and organized and proved that more than two centuries later a government of the people, by the people, and for the people has not perishe"d from the Earth. This is your victory."
+"And I know you didn't do this just to win an election. And I know you didn't do it for me. You did it because you understand the enormity of the task that lies ahead. For even as we celebrate tonight, we know the challenges that tomorrow will bring are the greatest of our lifetime: two wars, a planet in peril, the worst financial crisis in a century. Even as we stand here tonight, we know there are brave Americans waking up in the deserts of Iraq and the mountains of Afghanistan to risk their lives for us. There are mothers and fathers who will lie awake after the children fall asleep and wonder how they'll make the mortgage or pay their doctors' bills or save enough for their child's college education. There's new energy to harness, new jobs to be created, new schools to build, and threats to meet, alliances to repair."
+"The road ahead will be long. Our climb will be steep. We may not get there in one year or even in one term. But, America, I have never been more hopeful than I am tonight that we will get there. I promise you, we as a people will get there."
+"There will be setbacks and false starts. There are many who won't agree with every decision or policy I make as President. And we know the government can't solve every problem. But I will always be honest with you about the challenges we face. I will listen to you, especially when we disagree. And, above all, I will ask you to join in the work of remaking this nation, the only way it's been done in America for 221 years -- block by block, brick by brick, calloused hand by calloused hand. What began 21 months ago in the depths of winter cannot end on this autumn night."
+"This victory alone is not the change we seek. It is only the chance for us to make that change. And that cannot happen if we go back to the way things were. It can't happen without you, without a new spirit of service, a new spirit of sacrifice. So let us summon a new spirit of patriotism, of responsibility, where each of us resolves to pitch in and work harder and look after not only ourselves but each other. Let us remember that, if this financial crisis taught us anything, it's that we cannot have a thriving Wall Street while Main Street suffers. In this country, we rise or fall as one nation, as one people. Let's resist the temptation to fall back on the same partisanship and pettiness and immaturity that has poisoned our politics for so long."
+"Let's remember that it was a man from this state who first carried the banner of the Republican Party to the White House, a Party founded on the values of self-reliance and individual liberty and national unity. Those are values that we all share. And while the Democratic Party has won a great victory tonight, we do so with a measure of humility and determination to heal the divides that have held back our progress. As Lincoln said to a nation far more divided than ours: "We are not enemies but friends...." "Though passion may have strained, it must not break our bonds of affection."
+    speech2=["AMONG the vicissitudes incident to life no event could have filled me with greater anxieties than that of which the notification was transmitted by your order, and received on the 14th day of the present month. On the one hand, I was summoned by my country, whose voice I can never hear but with veneration and love, from a retreat which I had chosen with the fondest predilection, and, in my flattering hopes, with an immutable decision, as the asylum of my declining years - a retreat which was rendered every day more necessary as well as more dear to me by the addition of habit to inclination, and of frequent interruptions in my health to the gradual waste committed on it by time. On the other hand, the magnitude and difficulty of the trust to which the voice of my country called me, being sufficient to awaken in the wisest and most experienced of her citizens a distrustful scrutiny into his qualifications, could not but overwhelm with despondence one who (inheriting inferior endowments from nature and" 
+    "unpracticed in the duties of civil administration) ought to be peculiarly conscious of his own deficiencies. In this conflict of emotions all I dare aver is that it has been my faithful study to collect my duty from a just appreciation of every circumstance by which it might be affected. All I dare hope is that if, in executing this task, I have been too much swayed by a grateful remembrance of former instances, or by an affectionate sensibility to this transcendent proof of the confidence of my fellow-citizens, and have thence too little consulted my incapacity as well as disinclination for the weighty and untried cares before me, my error will be palliated by the motives which mislead me, and its consequences be judged by my country with some share of the partiality in which they originated."
+    "Such being the impressions under which I have, in obedience to the public summons, repaired to the present station, it would be peculiarly improper to omit in this first official act my fervent supplications to that Almighty Being who rules over the universe, who presides in the councils of nations, and whose providential aids can supply every human defect, that His benediction may consecrate to the liberties and happiness of the people of the United States a Government instituted by themselves for these essential purposes, and may enable every instrument employed in its administration to execute with success the functions allotted to his charge. In tendering this homage to the Great Author of every public and private good, I assure myself that it expresses your sentiments not less than my own, nor those of my fellow-citizens at large less than either. No people can be bound to acknowledge and adore the Invisible Hand which conducts the affairs of men more than those of the United States." 
+    "Every step by which they have advanced to the character of an independent nation seems to have been distinguished by some token of providential agency; and in the important revolution just accomplished in the system of their united government the tranquil deliberations and voluntary consent of so many distinct communities from which the event has resulted can not be compared with the means by which most governments have been established without some return of pious gratitude, along with an humble anticipation of the future blessings which the past seem to presage. These reflections, arising out of the present crisis, have forced themselves too strongly on my mind to be suppressed. You will join with me, I trust, in thinking that there are none under the influence of which the proceedings of a new and free government can more auspiciously commence."
+    "By the article establishing the executive department it is made the duty of the President to recommend to your consideration such measures as he shall judge necessary and expedient. The circumstances under which I now meet you will acquit me from entering into that subject further than to refer to the great constitutional charter under which you are assembled, and which, in defining your powers, designates the objects to which your attention is to be given. It will be more consistent with those circumstances, and far more congenial with the feelings which actuate me, to substitute, in place of a recommendation of particular measures, the tribute that is due to the talents, the rectitude, and the patriotism which adorn the characters selected to devise and adopt them. In these honorable qualifications I behold the surest pledges that as on one side no local prejudices or attachments, no separate views nor party animosities, will misdirect the comprehensive and equal eye which ought to watch" 
+    "over this great assemblage of communities and interests, so, on another, that the foundation of our national policy will be laid in the pure and immutable principles of private morality, and the preeminence of free government be exemplified by all the attributes which can win the affections of its citizens and command the respect of the world. I dwell on this prospect with every satisfaction which an ardent love for my country can inspire, since there is no truth more thoroughly established than that there exists in the economy and course of nature an indissoluble union between virtue and happiness; between duty and advantage; between the genuine maxims of an honest and magnanimous policy and the solid rewards of public prosperity and felicity; since we ought to be no less persuaded that the propitious smiles of Heaven can never be expected on a nation that disregards the eternal rules of order and right which Heaven itself has ordained; and since the preservation of the sacred fire of liberty" 
+    "and the destiny of the republican model of government are justly considered, perhaps, as deeply, as finally, staked on the experiment entrusted to the hands of the American people." 
+    "Besides the ordinary objects submitted to your care, it will remain with your judgment to decide how far an exercise of the occasional power delegated by the fifth article of the Constitution is rendered expedient at the present juncture by the nature of objections which have been urged against the system, or by the degree of inquietude which has given birth to them. Instead of undertaking particular recommendations on this subject, in which I could be guided by no lights derived from official opportunities, I shall again give way to my entire confidence in your discernment and pursuit of the public good; for I assure myself that whilst you carefully avoid every alteration which might endanger the benefits of an united and effective government, or which ought to await the future lessons of experience, a reverence for the characteristic rights of freemen and a regard for the public harmony will sufficiently influence your deliberations on the question how far the former can be impregnably fortified or" 
+    "the latter be safely and advantageously promoted." 
+    "To the foregoing observations I have one to add, which will be most properly addressed to the House of Representatives. It concerns myself, and will therefore be as brief as possible. When I was first honored with a call into the service of my country, then on the eve of an arduous struggle for its liberties, the light in which I contemplated my duty required that I should renounce every pecuniary compensation. From this resolution I have in no instance departed; and being still under the impressions which produced it, I must decline as inapplicable to myself any share in the personal emoluments which may be indispensably included in a permanent provision for the executive department, and must accordingly pray that the pecuniary estimates for the station in which I am placed may during my continuance in it be limited to such actual expenditures as the public good may be thought to require." 
+    "Having thus imparted to you my sentiments as they have been awakened by the occasion which brings us together, I shall take my present leave; but not without resorting once more to the benign Parent of the Human Race in humble supplication that, since He has been pleased to favor the American people with opportunities for deliberating in perfect tranquillity, and dispositions for deciding with unparalleled unanimity on a form of government for the security of their union and the advancement of their happiness, so His divine blessing may be equally conspicuous in the enlarged views, the temperate consultations, and the wise measures on which the success of this Government must depend." 
+
+    ]
+    d=dict()
+    for i in speech[0].split():
+            if i not in d:
+                    d[i]=1
+            else:
+                    d[i]=d[i]+1
+    d2=dict()
+
+    for i in speech2[0].split():
+            if i not in d2:
+                    d2[i]=1
+            else:
+                    d2[i]=d2[i]+1
+
+    sorted_d=sorted(d.items(), key=operator.itemgetter(1))
+    sorted_d2=sorted(d2.items(), key=operator.itemgetter(1))
+    print "speech"
+    print ""
+    for i in range (len(d)-1,len(d)-11,-1):
+        print sorted_d[i]
+    print ""
+    print "speech2"
+    print ""
+    for i in range (len(d2)-1,len(d2)-11,-1):
+        print sorted_d2[i]
+def seoul():
+    x=list()
+    x2=list()
+    x=[["very good","good","bad","so bad"], 
+    [13.1,37.1,1.5,3.52],  
+    [10.6,34.6,  1.9, 3.39],  
+    [27.1 ,40.0 ,1.5, 3.88],  
+    [16.2 , 37.8 ,  0.8 , 3.62],  
+    [11.4,  29.8   ,4.9 , 3.28],  
+    [12.2,  26.5  , 4.4 , 3.27],  
+    [13.5,  29.7,  2.4,  3.41],  
+    [13.7,  37.6 ,  1.2  ,3.59]]
+
+    x2=x[1:]
+    vg=0
+    g=0
+    b=0
+    sb=0
+    for i in range(len(x2)-1):
+        vg+=x2[i][0]
+        g+=x2[i][1]
+        b+=x2[i][2]
+        sb=x2[i][3]
+    goodavg=(vg+g)/len(x2)
+    badavg=(b+sb)/len(x2)
+    
+    
+    print "good avg"
+    print goodavg
+    print "bad avg"
+    print badavg
+    return goodavg,badavg
+def lab11():
+	seoul()
+	Speech()
+def main():
+    lab11()
+if __name__=="__main__": 
+     main()
