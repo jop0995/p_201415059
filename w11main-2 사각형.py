bcec4886f7ca60c458451d82fb87d303f653f198
201415059 _장민섭 <jms0995@hanmail.net>
2016-05-17 23:12:21 +0900

    Add files via upload

---
+++ w11main-2 사각형.py
@@ -0,0 +1,49 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+coord = [(100,100),(200,200)]
+curpos=t1.pos()
+xs=coord[0][0]
+ys=coord[0][1]
+xe=coord[1][0]
+ye=coord[1][1]
+
+def draw():
+    t1.penup()
+    t1.goto(100,100)
+    t1.pendown()
+    for i in range(0,4):
+        t1.fd(100)
+        t1.left(90)
+    t1.penup()
+    t1.home()
+def isinRectangle(coord,curpos):
+    if xs<=curpos[0]<=xe and ys<=curpos[1]<=ye:
+        t1.color("red")
+        draw()
+draw()
+def keyup():
+    t1.fd(50)
+    curpos=t1.pos()
+    isinRectangle(coord,curpos)
+def keydown():
+    t1.back(50)
+def turnr():
+    t1.right(90)
+def turnl():
+    t1.left(90)
+def mousegoto(x,y):
+    t1.setpos(x,y)
+    curpos=t1.pos()
+    isinRectangle(coord,curpos)
+def addkeys():
+    wn.onkey(keyup, "Up")
+    wn.onkey(keydown, "Down")
+    wn.onkey(turnr, "Right")
+    wn.onkey(turnl, "Left")
+def addmouse():
+    wn.onclick(mousegoto)
+addkeys()
+addmouse()
+wn.listen()
+turtle.mainloop()
---
+++ w11main-3 선그리기.py
@@ -0,0 +1,49 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def draw():
+    t1.penup()
+    t1.goto(50,100)
+    t1.pendown()
+    t1.fd(100)
+    t1.penup()
+    t1.home()
+
+draw()
+line1=[(50,100),(150,100)]
+x1=line1[0][0]-1
+y1=line1[0][1]-1
+x2=line1[1][0]+1
+y2=line1[1][1]+1
+pos1=(x1,y1)
+pos2=(x2,y2)
+
+def isOnLine(point,pos1,pos2):
+    if x1<=point[0]<=x2 and y1<=point[1]<=y2:
+        t1.color("red")
+        draw()
+def keyup():
+    t1.fd(50)
+    point=t1.pos()
+    isOnLine(point,pos1,pos2)
+def keydown():
+    t1.back(50)
+def turnr():
+    t1.right(90)
+def turnl():
+    t1.left(90)
+def mousegoto(x,y):
+    t1.setpos(x,y)
+    point=t1.pos()
+    isOnLine(point,pos1,pos2)
+def addkeys():
+    wn.onkey(keyup, "Up")
+    wn.onkey(keydown, "Down")
+    wn.onkey(turnr, "Right")
+    wn.onkey(turnl, "Left")
+def addmouse():
+    wn.onclick(mousegoto)
+addkeys()
+addmouse()
+wn.listen()
+turtle.mainloop()
