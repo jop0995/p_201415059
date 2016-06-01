50cb2287f5fbf5d165f2021a6b22d4e3b890fa43
201415059 _장민섭 <jms0995@hanmail.net>
2016-06-01 15:55:49 +0900

    dog mung mung 출력 하기

---
+++ w14main.py
@@ -0,0 +1,27 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is", self.name
+    def talk(self):
+        print "{0} sounds like mung mung...".format(self.name)
+        
+class ShihTzudog(object):
+    def __init__(self,name):
+        self.name=name     
+    def talk(self):
+        print "{0} sounds like si si ...".format(self.name)
+  
+class Maltese(object):
+    def __init__(self,name):
+        self.name=name     
+    def talk(self):
+        print "{0} sounds like mal mal ...".format(self.name)
+  
+        
+m=Dog('puppy')
+m.talk()
+m1=ShihTzudog('sichu')
+m1.talk()
+m2=Maltese('maltis')
+m2.talk()
